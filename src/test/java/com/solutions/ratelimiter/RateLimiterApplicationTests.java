e1819f3bad465c13f39ef02406efcd94e3d8ca38
Sagar Kanojia <isagarkanojia@gmail.com>
2022-04-03 22:53:14 +0530

    add project files

---
+++ src/test/java/com/solutions/ratelimiter/RateLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutions.ratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimiterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
